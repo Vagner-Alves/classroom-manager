47239552224e70e41d6af47cf18298a7c58e3b95
vagner-alves <vagneralves997@gmail.com>
2024-03-18 14:20:33 -0300

    primeiro commit

---
+++ classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.palmares.classroommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroommanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroommanagerApplication.class, args);
+	}
+
+}
---
+++ classroommanager/src/test/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.palmares.classroommanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroommanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
