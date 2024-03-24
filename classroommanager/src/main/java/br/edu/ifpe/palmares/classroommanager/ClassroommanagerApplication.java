eed9a734cfc36f3c6ede1f17411a17a084f2fd14
vagner-alves <vagneralves997@gmail.com>
2024-03-24 09:07:51 -0300

    dependecy injection

--- classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
+++ classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClassroommanagerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ClassroommanagerApplication.class, args);
 	}
 
