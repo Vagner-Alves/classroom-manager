7f0e9f4e8a035ea9edfb6f7c840aac4c6ef07a29
vagner-alves <vagneralves997@gmail.com>
2024-03-24 20:37:24 -0300

    jpa

--- classroommanager/src/main/java/com/packt/cardatabase/domain/Car.java
+++ classroommanager/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -4,13 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.*;
-
-
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+
 public class Car {
     @Id
     @GeneratedValue
