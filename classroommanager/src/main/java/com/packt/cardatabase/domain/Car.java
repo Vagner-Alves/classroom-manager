b73c25da59e4b0ea2041d4ae3afcaf9ff19a5d28
vagner-alves <vagneralves997@gmail.com>
2024-03-24 20:13:15 -0300

    jpa

--- classroommanager/src/main/java/com/packt/cardatabase/domain/Car.java
+++ classroommanager/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,5 +1,6 @@
 package com.packt.cardatabase.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -12,5 +13,7 @@ public class Car {
     private String brand, model, color, registrationNumber;
     private int modelYear, price;
 
+    @Column(name="explanation",nullable =true, length =512)
+    private String description;
 
 }
