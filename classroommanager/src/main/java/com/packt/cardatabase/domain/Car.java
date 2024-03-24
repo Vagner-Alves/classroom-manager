506f3addfd6ca943a6d84b0f427159aa5854847e
vagner-alves <vagneralves997@gmail.com>
2024-03-24 20:56:00 -0300

    jpa

--- classroommanager/src/main/java/com/packt/cardatabase/domain/Car.java
+++ classroommanager/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -4,16 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.awt.*;
+
 
 @Entity
-@Getter
-@Setter
+
 public class Car {
     @Id
     @GeneratedValue
     private long id;
+
     private String brand, model, color, registrationNumber;
     private int modelYear, price;
 
@@ -30,6 +31,55 @@ public class Car {
         this.color = color;
         this.registrationNumber = registrationNumber;
         this.modelYear = modelYear;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public  void setColor(String color){
+        this.color = color;
+    }
+
+    public String registrationNumber(){
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber){
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getModelYear(){
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear){
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
         this.price = price;
     }
 
