be02b7bb83fe086221100edb09174e4b4bb239f7
vagner-alves <vagneralves997@gmail.com>
2024-03-25 23:17:06 -0300

    jpa- crud repository

--- classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
+++ classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
@@ -5,8 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
-import com.packt.cardatabase.domain.Car;
-import com.packt.cardatabase.domain.CarRepository;
+import br.edu.ifpe.palmares.classroommanager.domain.Car;
+import br.edu.ifpe.palmares.classroommanager.domain.CarRepository;
 
 @SpringBootApplication
 public class ClassroommanagerApplication implements CommandLineRunner {
@@ -25,4 +25,20 @@ public class ClassroommanagerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {}
+
+
+		// CardataseApplication.java run method
+		@Override
+		public void run(String... args) throws Exception {
+			repository.save(new Car("Ford", "Mustang", "Red",
+					"ADF-1121", 2023, 59000));
+			repository.save(new Car("Nissan", "Leaf", "White",
+					"SSJ-3002", 2020, 29000));
+			repository.save(new Car("Toyota", "Prius",
+					"Silver", "KKO-0212", 2022, 39000));
+			// Fetch all cars and log to console
+			for (Car car : repository.findAll()) {
+				logger.info("brand: {}, model: {}",
+						car.getBrand(), car.getModel());
+			}
 }
