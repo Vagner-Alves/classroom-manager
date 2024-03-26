3299d96ad76b706c107c5d626ebe07dc5f196122
vagner-alves <vagneralves997@gmail.com>
2024-03-25 23:09:43 -0300

    jpa- crud repository

--- classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
+++ classroommanager/src/main/java/br/edu/ifpe/palmares/classroommanager/ClassroommanagerApplication.java
@@ -1,8 +1,12 @@
 package br.edu.ifpe.palmares.classroommanager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
 
 @SpringBootApplication
 public class ClassroommanagerApplication implements CommandLineRunner {
